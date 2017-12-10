84d23ed2e43eb06c119e93fad49ec32b281c987b
fuqua79 <mmugglin@hotmail.com>
2017-12-10 21:22:29 +0100

    GUI Anpassungen gemacht: Zutaten hinzufügen klappt JETZT !!!

--- src/app/rezept-erfassen/rezept-erfassen.component.ts
+++ src/app/rezept-erfassen/rezept-erfassen.component.ts
@@ -6,6 +6,7 @@ import {Rezept} from "../rezept/dto/rezept";
 import {RezeptService} from "../rezept/rezept.service";
 import * as model from "../rezept/dto/model-interfaces";
 
+
 @Component({
   selector: 'app-rezept-erfassen',
   templateUrl: './rezept-erfassen.component.html',
@@ -16,6 +17,7 @@ export class RezeptErfassenComponent implements OnInit {
   private id: string;
   public rezept: Rezept;
   public model = model;
+
   routeSubscription: Subscription;
 
   constructor(private route: ActivatedRoute,
@@ -35,37 +37,29 @@ export class RezeptErfassenComponent implements OnInit {
         this.rezeptService.loadRezept(this.id).subscribe(rezept => {
           this.rezept = rezept;
         });
+        // this.rezept$ = this.rezeptService.loadRezept(this.id);
 
       } else {
         console.log('Rezept neu erfassen');
         this.rezept = new Rezept();
+
       }
     });
   }
 
   addZutat(): void {
-    console.log("this.rezept.zutaten", this.rezept.zutaten);
-    setTimeout(function(){ }, 2000);
-    this.rezept.zutaten.push(new Zutat());
-    setTimeout(function(){ }, 2000);
-    console.log("NACH NEU this.rezept.zutaten", this.rezept.zutaten);
+    let zutat = new Zutat();
+    this.rezept.zutaten.push(zutat);
   }
 
-  removeZutat(arrayIndex: number): void {
-    console.log('zutaten vor loschen: ', this.rezept.zutaten);
-    console.log('zutaten vor loschen length: ', this.rezept.zutaten.length);
-    console.log('arrayIndex: ', arrayIndex);
+  removeZutat(arrayIndex: number): boolean {
     this.rezept.zutaten.splice(arrayIndex, 1);
-    console.log('zutaten nach loschen: ', this.rezept.zutaten);
-    console.log('zutaten nach loschen length: ', this.rezept.zutaten.length);
+    return false;
   }
 
   saveRezept(rezept: Rezept): void {
     //Speichern...
-    console.log('Speichere Rezept...');
-
     this.rezeptService.saveRezept(rezept).subscribe(result => {
-      console.log('Rezept erfolgreich gespeichert');
     });
   }
 
@@ -97,5 +91,6 @@ export class RezeptErfassenComponent implements OnInit {
     this.rezept.imageFilename = file;
     // let fileName = file.name;
   }
+
 }
 
