26456e5d2942bf0cefed40b19cf195376d72000c
fuqua79 <mmugglin@hotmail.com>
2019-09-29 16:17:53 +0200

    multer in eigenes file extrahiert

--- server/routes/rezepte.js
+++ server/routes/rezepte.js
@@ -1,40 +1,13 @@
 const express = require("express");
-const multer = require('multer');
 
 const RezeptController = require('../controllers/rezepte');
 const checkAuth = require('../middleware/check-auth');
+const extractFile = require('../middleware/file');
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg"
-};
-
-
-// Helper-Logik zum File speichern
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime type");
-    if (isValid) {
-      error = null;
-    }
-    cb(error, "server/assets/images");
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname
-      .toLowerCase()
-      .split(" ")
-      .join("-");
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + "." + ext);
-  }
-});
-
 //FILE speichern
-router.post('/file/save', checkAuth, multer({storage: storage}).single("image"), RezeptController.saveFile);
+router.post('/file/save', checkAuth, extractFile, RezeptController.saveFile);
 
 //Rezept INSERTEN
 router.post('/save', checkAuth, RezeptController.insertRezept);
@@ -45,10 +18,10 @@ router.put('/:id', checkAuth, RezeptController.updateRezept);
 //ALLE Rezepte LADEN
 router.get('/list', RezeptController.getAllRezepte);
 
-//Random Rezept lesen
+//Random Rezept laden
 router.get('/random', RezeptController.getRandomRezept);
 
-//Rezept LADEN
+//Einzelnes Rezept LADEN
 router.get('/:id', RezeptController.getRezept);
 
 //Rezept LOESCHEN
--- server/routes/user.js
+++ server/routes/user.js
@@ -4,8 +4,12 @@ const UserController = require("../controllers/user");
 
 const router = express.Router();
 
+
 router.post("/signup", UserController.createUser);
+
 router.post("/login", UserController.login);
+
 router.get('/:id', UserController.getUser);
+
 
 module.exports = router;
